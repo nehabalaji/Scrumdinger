5ac8712c4093b86372034bb1362b7b9d7d1aa5db
Neha Balaji <55323591+nehabalaji@users.noreply.github.com>
2024-02-15 00:42:10 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Neha on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

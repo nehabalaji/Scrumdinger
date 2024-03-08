9792db24a679d4c2a2cb0d3ac446ed2d9c08fd3b
nehabalaji <nehabalaji10@gmail.com>
2024-03-07 20:09:03 -0500

    create a card view

--- Scrumdinger/Cardview.swift
+++ Scrumdinger/Cardview.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct Cardview: View {
+    let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+                .accessibilityAddTraits(/*@START_MENU_TOKEN@*/.isHeader/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
+                Spacer()
+                Label(
+                    title: { Text("\(scrum.lenghtInMinutes)") },
+                    icon: { Image(systemName: "clock") }
+                )
+                .padding(.trailing, 20)
+                .accessibilityLabel("\(scrum.lenghtInMinutes) minute meeting")
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
 #Preview {
-    Cardview()
+    var scrum = DailyScrum.sampleData[0]
+    return Cardview(scrum: scrum)
+        .background(scrum.theme.mainColor)
+        .previewLayout(.fixed(width: 400, height: 60))
 }
--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,19 @@
 //  Created by Neha on 3/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self==TrailingIconLabelStyle{
+    static var trailingIcon: Self {
+        Self()
+    }
+}
